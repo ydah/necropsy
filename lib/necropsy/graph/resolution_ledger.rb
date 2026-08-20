41b05771e404235cc1b18ce246fb1751d990cc67
ydah <t.yudai92@gmail.com>
2026-08-20 11:06:41 +0900

    Make graph ledgers independently loadable

--- lib/necropsy/graph/resolution_ledger.rb
+++ lib/necropsy/graph/resolution_ledger.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'resolution_store'
+
 module Necropsy
   module Graph
     class ResolutionLedger
--- spec/necropsy/graph/call_graph_load_spec.rb
+++ spec/necropsy/graph/call_graph_load_spec.rb
@@ -4,16 +4,26 @@ require 'open3'
 require 'rbconfig'
 
 RSpec.describe 'CallGraph dependency boundary' do
-  it 'loads the graph boundary without loading the complete public API' do
+  def load_graph_component(component)
     lib = File.expand_path('../../../lib', __dir__)
     _stdout, stderr, status = Open3.capture3(
       RbConfig.ruby,
       '-I',
       lib,
       '-e',
-      'require "necropsy/graph/call_graph"'
+      "require \"necropsy/graph/#{component}\""
     )
 
     expect(status).to be_success, stderr
+  end
+
+  it 'loads the graph boundary without loading the complete public API' do
+    load_graph_component('call_graph')
+  end
+
+  %w[evidence_ledger resolution_ledger].each do |component|
+    it "loads #{component} without relying on CallGraph load order" do
+      load_graph_component(component)
+    end
   end
 end
